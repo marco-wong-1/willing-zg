79235ecc889af2f5dadf3db6dd021875646a4b60
JCog <42006114+JCog@users.noreply.github.com>
2020-06-18 18:30:10 -0400

    create all_components component and setup github test-output workflow (#14)
    * create all_components component and setup github test-output workflow
    
    * revert .gitignore
    
    * import instantiated component instances instead of component classes for all_components

--- willing_zg/__init__.py
+++ willing_zg/__init__.py
@@ -1,4 +1,5 @@
 from . import resources
+from .all_components import all_components
 from .custom_server import custom_server
 from .deployment import deployment
 from .tokens import token_util
@@ -6,6 +7,6 @@ from .simple_jwt import simple_jwt
 from importlib_metadata import version
 from .email import email
 
-__all__ = ["resources", "custom_server", "deployment", "token_util", "simple_jwt", "email"]
+__all__ = ["all_components", "resources", "custom_server", "deployment", "token_util", "simple_jwt", "email"]
 
 __version__ = version("willing_zg")
