1a266ff21f63fcc401c1d1e3e141a308d77ea5c7
Ian MacLeod <ian.a.macleod@gmail.com>
2020-07-24 13:06:47 -0700

    Add get_env component

--- willing_zg/__init__.py
+++ willing_zg/__init__.py
@@ -7,6 +7,7 @@ from importlib_metadata import version
 from .email import email
 from .chat import chat
 from .django_willing_zg import django_willing_zg
+from .get_env import get_env_util
 
 
 __all__ = [
@@ -18,6 +19,7 @@ __all__ = [
     "email",
     "chat",
     "django_willing_zg",
+    "get_env_util",
 ]
 
 __version__ = version("willing_zg")
