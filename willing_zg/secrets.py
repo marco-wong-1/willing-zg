43d488d60bd17888b3e7fcba5b7a5329f0404cff
Cathy Song <cathy@willing.com>
2020-08-13 15:07:01 -0700

    Remove backslash escape from string

--- willing_zg/secrets.py
+++ willing_zg/secrets.py
@@ -6,22 +6,22 @@ log = logging.getLogger()
 
 # Get secrets from serets manager via get_secret() function
 
-database_secret = """if \"DATABASE_SECRET\" in os.environ:
-    db_secret = json.loads(get_secret(os.environ[\"DATABASE_SECRET\"])[\"SecretString\"])
+database_secret = """if "DATABASE_SECRET" in os.environ:
+    db_secret = json.loads(get_secret(os.environ["DATABASE_SECRET"])["SecretString"])
 
-    db_username = db_secret[\"username\"]
-    db_password = db_secret[\"password\"]
-    db_host = db_secret[\"host\"]
-    db_port = str(db_secret[\"port\"])
-    db_clusterid = db_secret[\"dbClusterIdentifier\"]
+    db_username = db_secret["username"]
+    db_password = db_secret["password"]
+    db_host = db_secret["host"]
+    db_port = str(db_secret["port"])
+    db_clusterid = db_secret["dbClusterIdentifier"]
 
-    db_url = f\"postgres://{db_username}:{db_password}@{db_host}:{db_port}/{db_clusterid}\"
-    os.environ[\"DATABASE_URL\"] = db_url
+    db_url = f"postgres://{db_username}:{db_password}@{db_host}:{db_port}/{db_clusterid}"
+    os.environ["DATABASE_URL"] = db_url
 """
 
-email_host_password = """if \"DJANGO_EMAIL_HOST_PASSWORD\" in os.environ:
-    django_password = json.loads(get_secret(os.environ[\"DJANGO_EMAIL_HOST_PASSWORD\"])[\"SecretString\"])
-    EMAIL_HOST_PASSWORD = django_password[\"DJANGO_EMAIL_HOST_PASSWORD\"]
+email_host_password = """if "DJANGO_EMAIL_HOST_PASSWORD" in os.environ:
+    django_password = json.loads(get_secret(os.environ["DJANGO_EMAIL_HOST_PASSWORD"])["SecretString"])
+    EMAIL_HOST_PASSWORD = django_password["DJANGO_EMAIL_HOST_PASSWORD"]
 """
 
 
