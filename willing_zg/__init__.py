4c8d7c6c2d3f120cc90aed93281fa054b0ae6a10
JCog <jordancogan4@gmail.com>
2020-09-16 18:05:04 -0400

    update list of all components

--- willing_zg/__init__.py
+++ willing_zg/__init__.py
@@ -6,6 +6,7 @@ from .tokens import token_util
 from importlib_metadata import version
 from .django_willing_zg import django_willing_zg
 from .google_analytics import google_analytics
+from .chat import chat
 
 __all__ = [
     "all_components",
@@ -13,9 +14,9 @@ __all__ = [
     "get_env_util",
     "deployment",
     "token_util",
-    "simple_jwt",
     "django_willing_zg",
     "google_analytics",
+    "chat"
 ]
 
 __version__ = version("willing_zg")
